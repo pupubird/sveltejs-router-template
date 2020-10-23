95e0277be97d7a7db64e3ea4a81f58a3b278eeac
pupubird <rainchai4240@gmail.com>
2020-10-24 03:24:21 +0800

    Update order, extras should come before routes

--- src/utils/generateRouter.js
+++ src/utils/generateRouter.js
@@ -1,13 +1,13 @@
 function Router(routes, extras) {
     // Generate a function that accepts a base parameter to append to routes
     return function (base) {
-        routes = routes.map(route => {
-            route[0] = base + route[0]
-            return route
-        })
         extras && extras.map(r => {
             let router = r[1](base + r[0]);
             routes = [...routes, ...router];
+        })
+        routes = routes.map(route => {
+            route[0] = base + route[0]
+            return route
         })
         return routes
     }
